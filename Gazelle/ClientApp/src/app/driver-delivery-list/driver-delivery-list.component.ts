fac1962ea41a2b02d2736ffe9c1b7f8843334ab4
Rune Lundegaard Uggerhøj <rlu@netcompany.com>
2020-06-11 21:38:41 +0200

    httpGet work

--- Gazelle/ClientApp/src/app/driver-delivery-list/driver-delivery-list.component.ts
+++ Gazelle/ClientApp/src/app/driver-delivery-list/driver-delivery-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { DeliveryListService } from '../delivery-list.service';
 
 @Component({
   selector: 'app-driver-delivery-list',
   templateUrl: './driver-delivery-list.component.html',
   styleUrls: ['./driver-delivery-list.component.css']
 })
-export class DriverDeliveryListComponent implements OnInit {
+export class DriverDeliveryListComponent implements OnInit{
 
-  constructor() { }
+  constructor(private deliveryListService: DeliveryListService) {}
+
+  public deliveries = this.deliveryListService.getDeliveryList();
 
   ngOnInit() {
+    //this.buildTableRows();
   }
 
+  /**buildTableRows() {
+    var table = <HTMLTableElement> document.getElementById("driverDeliveryTableBody");
+    var row = table.insertRow(0);
+    var titleCellPackageId = row.insertCell(0);
+    var titleCellOriginCity = row.insertCell(1);
+    var titleCellDestinationCity = row.insertCell(2);
+
+    let listOfDeliveries = this.deliveryListService.getDeliveryList();
+
+    titleCellPackageId.innerHTML = "1-placeholder";
+    titleCellOriginCity.innerHTML = "2-placeholders";
+    titleCellDestinationCity.innerHTML = "3-happy-little-placeholders";
+  }*/
 }
