8a7ac444905c9853e4e3fe158eebd5f081295b54
Niels Lyngs Ladekarl <nll@netcompany.com>
2020-06-11 15:29:17 +0200

    Added Deliveries

--- Gazelle/Controllers/ConnectionsController.cs
+++ Gazelle/Controllers/ConnectionsController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Gazelle.Models;
+using System;
 
 namespace Gazelle.Controllers
 {
@@ -12,17 +13,38 @@ namespace Gazelle.Controllers
     {
         private readonly GazelleContext _context;
 
+        private readonly string[] _deliveryTypes = new string[] { "frozen", "fragile", "weapon", "recordedDelivery", "animal" };
+        private readonly string[] _supportedDeliveryTypes = new string[] { "frozen", "fragile", "recordedDelivery", "animal" };
+
         public ConnectionsController(GazelleContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public async Task<ActionResult<Connection>> Get(string origin, string destination, int weight, int length, int height, int depth, bool frozen, bool fragile, bool weapon, bool recordedDelivery, bool animal, bool recommendedDelivery)
+        public async Task<ActionResult<Connection>> Get(string origin, string destination, int weight, int length, int height, int depth, string[] deliveryTypes)
         {
-            if(weight > 40 || weapon)
+            if(weight > 40)
             {
                 return NotFound();
+            }
+
+            foreach(var deliveryType in deliveryTypes)
+            {
+                var substrings = deliveryType.Split("=");
+                var key = substrings[0];
+                var value = bool.Parse(substrings[1]);
+
+                var knownDeliveryType = _deliveryTypes.FirstOrDefault(x => x == key);
+                if(knownDeliveryType == null)
+                {
+                    return NotFound();
+                }
+
+                if (value && !_supportedDeliveryTypes.Contains(knownDeliveryType))
+                {
+                    return NotFound();
+                }
             }
 
             var fromCityInvariant = origin.ToLower();
---
+++ Gazelle/Controllers/DeliveriesController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Gazelle.Models;
+
+namespace Gazelle.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeliveriesController : ControllerBase
+    {
+        private readonly GazelleContext _context;
+
+        public class CreateDeliveryModel
+        {
+            public string OriginName { get; set; }
+            public string DestinationName { get; set; }
+            public int DeliveryType { get; set; }
+            public double Size { get; set; }
+            public Route ApprovedRoute { get; set; }
+        }
+
+        public class ApproveDeliveryModel
+        {
+            public int DeliveryId { get; set; }
+            public int ApprovedRouteId { get; set; }
+        }
+
+        public DeliveriesController(GazelleContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Delivery>>> GetDeliveries()
+        {
+            return await _context.Deliveries.ToListAsync();
+        }
+
+        [HttpGet(Name = "Routes")]
+        public async Task<ActionResult<Route>> GetRoutes(string origin, string destination, string weight)
+        {
+            var routes = new List<Route>();
+
+            var connections = new List<Connection>();
+            connections.Add(new Connection
+            {
+                ConnectionId = 1,
+                Price = 50,
+                Time = 500,
+                Company = "Telstar",
+                StartCity = new City { CityId = Guid.NewGuid(), CityName = "Dakar", Country = new Country { CountryId = 1, Conflict = false, Name = "Marokko" } },
+                EndCity = new City
+                {
+                    CityId = Guid.NewGuid(),
+                    CityName = "Marakkesh",
+                    Country = new Country { CountryId = 1, Conflict = false, Name = "Marokko" },
+                }
+            });
+
+            routes.Add(new Route
+            {
+                RouteId = 1,
+                Price = 50,
+                Time = 500,
+                Companies = "Telstar,EastIndia",
+                Connections = connections
+            });
+
+            return Ok(routes);
+        }
+
+        [HttpPost(Name = "Approve")]
+        public async Task<ActionResult<Delivery>> ApproveDelivery([FromBody] ApproveDeliveryModel approveDeliveryModel)
+        {
+            var delivery = await _context.Deliveries
+                .Include(x => x.Routes)
+                .FirstOrDefaultAsync(x => x.DeliveryId == approveDeliveryModel.DeliveryId);
+
+            if (delivery == null)
+            {
+                return NotFound();
+            }
+
+            var approvedRoute = delivery.Routes.FirstOrDefault(x => x.RouteId == approveDeliveryModel.ApprovedRouteId);
+
+            if (approvedRoute == null)
+            {
+                return NotFound();
+            }
+
+            delivery.ApprovedRoute = approvedRoute;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(delivery);
+        }
+    }
+}
