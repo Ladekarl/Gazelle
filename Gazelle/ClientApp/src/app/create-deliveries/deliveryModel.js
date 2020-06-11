b32b7bd32a19c54782eaaba9aa59cce95fbcd9d6
asmw <asmw@netcompany.com>
2020-06-11 22:32:53 +0200

    cities controller

---
+++ Gazelle/ClientApp/src/app/create-deliveries/deliveryModel.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Delivery = /** @class */ (function () {
+    function Delivery() {
+    }
+    return Delivery;
+}());
+exports.Delivery = Delivery;
+//# sourceMappingURL=deliveryModel.js.map
